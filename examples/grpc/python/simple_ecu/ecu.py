fe0230b2195d887f19d178a6cfb4db448b2c9e41
Aleksandar Filipov <aleksandar.f@gmail.com>
2020-10-12 16:52:06 +0200

    cleaning

--- examples/grpc/python/simple_ecu/ecu.py
+++ examples/grpc/python/simple_ecu/ecu.py
@@ -68,30 +68,6 @@ def reload_configuration(system_stub):
 
 ##################### END BOILERPLATE ####################################################
 
-def publish_signals(client_id, stub, diag_frame_resp):
-    signal_with_payload = network_api_pb2.Signal(id = diag_frame_resp)
-    signal_with_payload.raw = b'\x01x02x03x04x05x06x07x08'
-    publisher_info = network_api_pb2.PublisherConfig(clientId = client_id, signals=network_api_pb2.Signals(signal=[signal_with_payload]), frequency = 0)
-    try:
-        stub.PublishSignals(publisher_info)
-    except grpc._channel._Rendezvous as err:
-        print(err)
-
-
-
-import binascii
-def subscribe_to_diag(client_id, stub, diag_frame_req, diag_frame_resp):
-    sub_info = network_api_pb2.SubscriberConfig(clientId=client_id, signals=network_api_pb2.SignalIds(signalId=[diag_frame_req]), onChange=False)
-    try:
-        for response in stub.SubscribeToSignals(sub_info):
-            print(response)   
-            print(binascii.hexlify(response.signal[0].raw))
-            # here we could switch on the response.signal[0].raw which contains service id and did.     
-            publish_signals(client_id, stub, diag_frame_resp, )
-    except grpc._channel._Rendezvous as err:
-            print(err)
-
-increasing_counter = 0
 
 def publish_signal(client_id, stub, signal, value):
     signal_with_payload = network_api_pb2.Signal(id = signal)
@@ -102,22 +78,7 @@ def publish_signal(client_id, stub, signal, value):
     except grpc._channel._Rendezvous as err:
         print(err)
 
-
-# def publish_on_timer(client_id, stub, signal, pause):
-#     global counter
-#     while True:
-        
-#         signal_with_payload = network_api_pb2.Signal(id = signal)
-#         signal_with_payload.integer = counter % 100
-#         publisher_info = network_api_pb2.PublisherConfig(clientId = client_id, signals=network_api_pb2.Signals(signal=[signal_with_payload]), frequency = 0)
-#         try:
-#                 stub.PublishSignals(publisher_info)
-#         except grpc._channel._Rendezvous as err:
-#                 print(err)
-#         time.sleep(pause)
-#         counter = counter + 1
-
-
+increasing_counter = 0
 # ecu_A publish some value (counter), read other value (counter_times_2) (which is published by ecu_B)
 def ecu_A(stub, pause):
     while True:
@@ -127,14 +88,13 @@ def ecu_A(stub, pause):
         counter = common_pb2.SignalId(name="counter", namespace=common_pb2.NameSpace(name = namespace))
         publish_signal(clientId, stub, counter, increasing_counter)
         
+        # read the other value and output result
         counter_times_2 = common_pb2.SignalId(name="counter_times_2", namespace=common_pb2.NameSpace(name = namespace))
         read_counter_times_2 = read_signal(stub, counter_times_2)
 
         print("counter_times_2 is ", read_counter_times_2.signal[0].integer)
         increasing_counter = increasing_counter + 1
         time.sleep(pause)
-
-        # read the other signal, print value
 
 # read some value (counter) published by ecu_a, double and send value (counter_times_2)
 def ecu_B(stub, pause):
@@ -148,23 +108,6 @@ def ecu_B(stub, pause):
         counter_times_2 = common_pb2.SignalId(name="counter_times_2", namespace=common_pb2.NameSpace(name = namespace))
         publish_signal(client_id, stub, counter_times_2, read_counter.signal[0].integer * 2)        
         time.sleep(pause)
-
-        # double the value and publish
-
-
-        # signal_counter_ecu_A = common_pb2.SignalId(name="counter", namespace=common_pb2.NameSpace(name = namespace))
-        # # counter = 
-
-        # publish_signal(client_id_ecu_A, stub, signal_counter_ecu_A, counter)
-        # counter = counter + 1
-        # time.sleep(pause)
-
-        # signal_double_ecu_B = common_pb2.SignalId(name="counter_times_2", namespace=common_pb2.NameSpace(name = namespace))
-        # double_signal = read_signal(stub, signal_double_ecu_B)
-
-        # print("Double signal is ", double_signal.signal[0].integer)
-        # read the other signal, print value
-
         
 def read_signal(stub, signal):
     read_info = network_api_pb2.SignalIds(signalId=[signal])
@@ -184,48 +127,18 @@ def read_on_timer(client_id, stub, signal, pause):
 
 def run():
     channel = grpc.insecure_channel('192.168.1.33:50051')
-#     channel = grpc.insecure_channel('192.168.4.1:50051')
     network_stub = network_api_pb2_grpc.NetworkServiceStub(channel)
     system_stub = system_api_pb2_grpc.SystemServiceStub(channel)
     
-
-# If you need to change the id of the req/resp modify here configuration/can/diagnostics.dbc 
-#     diag_frame_req = common_pb2.SignalId(name="DiagReqFrame_2016", namespace=common_pb2.NameSpace(name = "DiagnosticsCanInterface"))
-#     diag_frame_resp = signal = common_pb2.SignalId(name="DiagResFrame_2024", namespace=common_pb2.NameSpace(name = "DiagnosticsCanInterface"))
- 
     upload_folder(system_stub, "configuration")
     reload_configuration(system_stub)
 
-    # let ecu_A write value at some given intervall
-    
-#     client_id_ecu_A = common_pb2.ClientId(id="ecu_A")
-#     signal_on_ecu_A = common_pb2.SignalId(name="counter", namespace=common_pb2.NameSpace(name = "ecu_A"))
     ecu_A_thread  = Thread(target = ecu_A, args = (network_stub, 1,))
     ecu_A_thread.start()
 
     ecu_B_thread  = Thread(target = ecu_B, args = (network_stub, 1,))
     ecu_B_thread.start()
 
-#     ecu_A_thread = Thread(target = publish_on_timer, args = (client_id_ecu_A, network_stub, signal_on_ecu_A, 1, ))
-#     ecu_A_thread.start()
-
-    # let ecu_B read the value muliply by 2 and send back result
-#     client_id_ecu_B = common_pb2.ClientId(id="ecu_B")
-#     signal_on_ecu_B = common_pb2.SignalId(name="counter", namespace=common_pb2.NameSpace(name = "ecu_B"))
-#     ecu_B_thread = Thread(target = read_on_timer, args = (client_id_ecu_B, network_stub, signal_on_ecu_B, 1, ))
-#     ecu_B_thread.start()
-
-     
-
-#     print("-------------- Subscribe to diag_req, on request submit resp_frame --------------")
-#     subscribe_to_diag(client_id, network_stub, diag_frame_req, diag_frame_resp)
-
 
 if __name__ == '__main__':
     run()
-
-
-# try this by doing
-# 
-# {"command": "subscribe", "signals": ["DiagResFrame_2024"], "namespace" : "DiagnosticsCanInterface"}
-# {"command" : "write", "signals" : {"DiagReqFrame_2016": 3}, "namespace" : "DiagnosticsCanInterface"}
