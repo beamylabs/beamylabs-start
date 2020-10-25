c81372a21ce4cf0763a9fa0b17b45f80e4ed6755
Aleksandar Filipov <aleksandar.f@gmail.com>
2020-10-25 21:49:52 +0100

    Try to get IP address automatically

---
+++ scripts/resolve-ip.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eu
+
+defaultip="192.168.4.1"
+
+iface="${1:-}"
+
+if [ -z "$iface" ]; then
+  echo >/dev/stderr "no interface given"
+  echo "$defaultip"
+  exit 0
+fi
+
+found="$(ifconfig "$iface" | sed -E -n "s/^[[:space:]]*inet (([0-9]+\.){3}[0-9]+).*/\1/p")"
+case "$found" in
+  ""|127.0.0.1)
+    ip="$defaultip"
+    ;;
+  *)
+    ip="$found"
+    ;;
+esac
+
+echo "$ip"
--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-SIGNALBROKER_IP=192.168.4.1 docker-compose -f docker-compose-full-system.yml down
-SIGNALBROKER_IP=192.168.4.1 docker-compose -f docker-compose-full-system.yml pull
-SIGNALBROKER_IP=192.168.4.1 docker-compose -f docker-compose-full-system.yml up -d
+#!/bin/sh
+
+cd "${0%/*}"
+
+SIGNALBROKER_IP=$(scripts/resolve-ip.sh eth0) docker-compose -f docker-compose-full-system.yml down
+SIGNALBROKER_IP=$(scripts/resolve-ip.sh eth0) docker-compose -f docker-compose-full-system.yml pull
+SIGNALBROKER_IP=$(scripts/resolve-ip.sh eth0) docker-compose -f docker-compose-full-system.yml up -d
