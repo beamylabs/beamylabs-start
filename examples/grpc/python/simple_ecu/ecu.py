48334161e670497cfe222bb6eebcc9a22168be7b
Aleksandar Filipov <aleksandar.f@gmail.com>
2021-01-07 10:14:16 +0100

    inital lin commit

--- examples/grpc/python/simple_ecu/ecu.py
+++ examples/grpc/python/simple_ecu/ecu.py
@@ -149,7 +149,8 @@ def run():
     system_stub = system_api_pb2_grpc.SystemServiceStub(channel)
     check_license(system_stub)
     
-    upload_folder(system_stub, "configuration_udp")
+    # upload_folder(system_stub, "configuration_udp")
+    upload_folder(system_stub, "configuration_lin")
     # upload_folder(system_stub, "configuration")
     reload_configuration(system_stub)
 
