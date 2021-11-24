07f9127bc31ea3051afa101d8fa854476a220d62
Aleksandar Filipov <aleksandar.f@gmail.com>
2021-11-24 19:04:26 +0100

    use optional parmam in example

--- examples/grpc/python/simple_ecu/ecu.py
+++ examples/grpc/python/simple_ecu/ecu.py
@@ -103,7 +103,7 @@ def ecu_A(stub, pause):
                 ),
                 # add any number of signals here
                 # signal_creator.signal_with_payload(
-                #     "TestFr04", namespace, ("raw", binascii.unhexlify("0a0b0c0d"))
+                #     "TestFr04", namespace, ("raw", binascii.unhexlify("0a0b0c0d"), False)
                 # ),
             ],
         )
@@ -218,7 +218,7 @@ def double_and_publish(network_stub, client_id, trigger, signals):
                     ),
                     # add any number of signals/frames here
                     # signal_creator.signal_with_payload(
-                    #     "TestFr04", "ecu_B", ("raw", binascii.unhexlify("0a0b0c0d"))
+                    #     "TestFr04", "ecu_B", ("raw", binascii.unhexlify("0a0b0c0d"), False)
                     # )
                 ],
             )
