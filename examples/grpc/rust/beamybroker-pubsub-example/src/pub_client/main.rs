1535d5f2816c8862ce1e24265438139ebe28d02d
Niclas <niclas.lind93@gmail.com>
2022-01-08 13:59:06 +0100

    fix issue #68

--- examples/grpc/rust/beamybroker-pubsub-example/src/pub_client/main.rs
+++ examples/grpc/rust/beamybroker-pubsub-example/src/pub_client/main.rs
@@ -1,8 +1,13 @@
-use beamy_broker_client_example::beamy_api::base::network_service_client::NetworkServiceClient;
-use beamy_broker_client_example::beamy_api::base::{
-    signal::Payload::Integer, ClientId, NameSpace, PublisherConfig, Signal, SignalId, Signals,
-};
 use std::io::Write;
+
+use lib_helper::{
+    beamy_api::base::{
+        network_service_client::NetworkServiceClient, signal::Payload::Integer,
+        system_service_client::SystemServiceClient, ClientId, NameSpace, PublisherConfig, Signal,
+        SignalId, Signals,
+    },
+    check_license, reload_configuration, upload_folder,
+};
 use tonic::transport::Channel;
 
 #[tokio::main]
@@ -10,7 +15,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let channel = Channel::from_static("http://localhost:50051")
         .connect()
         .await?;
-    let mut client = NetworkServiceClient::new(channel);
+
+    let mut system_stub = SystemServiceClient::new(channel.clone());
+    let mut network_stub = NetworkServiceClient::new(channel);
+
+    check_license(&mut system_stub).await?;
+    upload_folder(&mut system_stub, "configuration").await?;
+    reload_configuration(&mut system_stub).await?;
 
     // Publish 10 messages
     for _ in 0..=10 {
@@ -35,7 +46,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             frequency: 0,
         };
 
-        let result = client.publish_signals(publisher_config).await?;
+        let result = network_stub.publish_signals(publisher_config).await?;
         println!("{:#?}", result.metadata());
     }
 
