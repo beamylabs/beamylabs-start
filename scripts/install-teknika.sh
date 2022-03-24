a0b8e3d105c94056fdb309d38efb4f426ee45e0f
Daniel Lublin <daniel@lublin.se>
2022-03-24 12:21:30 +0100

    Don't overwrite if user change teknika listen ports

--- scripts/install-teknika.sh
+++ scripts/install-teknika.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "${0%/*}"
+cd "${0%/*}" || exit 1
 
 if [[ "$EUID" != 0 ]]; then
   printf "Please give me root privileges by running me with sudo or doas.\n"
@@ -24,8 +24,15 @@ case "$arch" in
 esac
 
 mkdir -p /etc/teknika
-echo "PORT=51111" >/etc/teknika/instance1
-echo "PORT=51112" >/etc/teknika/instance2
+# We avoid overwriting any user changes to the ports
+if ! grep -sq "^PORT=" /etc/teknika/instance1; then
+  touch /etc/teknika/instance1
+  printf >/etc/teknika/instance1 "PORT=51111\n"
+fi
+if ! grep -sq "^PORT=" /etc/teknika/instance2; then
+  touch /etc/teknika/instance2
+  printf >/etc/teknika/instance2 "PORT=51112\n"
+fi
 
 sed >/etc/systemd/system/teknika@.service \
     -e "s,@TEKNIKABIN@,$TEKNIKABIN,g" \
