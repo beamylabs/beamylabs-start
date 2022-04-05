a46419d9f0b7356baa31794eb2c359dacbc9af56
Aleksandar Filipov <aleksandar.f@gmail.com>
2022-04-05 17:20:46 +0200

    relay code split client and host

--- examples/grpc/python/simple_ecu/relay_client.py
+++ examples/grpc/python/simple_ecu/relay_client.py
@@ -84,75 +84,6 @@ def printer(signals):
         print(f"{signal.id.name} {signal.id.namespace.name} {get_value(signal)}")
 
 
-def ecu_A(stub, pause):
-    """Publishes a value, read other value (published by ecu_B)
-
-    Parameters
-    ----------
-    stub : NetworkServiceStub
-        Object instance of class
-    pause : int
-        Amount of time to pause, in seconds
-
-    """
-    increasing_counter = 0
-    namespace = "ecu_A"
-    clientId = common_pb2.ClientId(id="id_ecu_A")
-    while True:
-
-        print("\necu_A, seed is ", increasing_counter)
-        # Publishes value 'counter'
-
-        publish_signals(
-            clientId,
-            stub,
-            [
-                signal_creator.signal_with_payload(
-                    "counter", namespace, ("integer", increasing_counter)
-                ),
-                # add any number of signals here, make sure that all signals/frames are unique.
-                # signal_creator.signal_with_payload(
-                #     "TestFr04", namespace, ("raw", binascii.unhexlify("0a0b0c0d")), False
-                # ),
-            ],
-        )
-
-        time.sleep(pause)
-
-        # Read the other value 'counter_times_2' and output result
-
-        read_signal_response = read_signals(
-            stub, signal_creator.signal("counter_times_2", namespace)
-        )
-        for signal in read_signal_response.signal:
-            print(f"ecu_A, (result) {signal.id.name} is {get_value(signal)}")
-        increasing_counter = (increasing_counter + 1) % 4
-
-
-def read_on_timer(stub, signals, pause):
-    """Simple reading with timer
-
-    Parameters
-    ----------
-    stub : NetworkServiceStub
-        Object instance of class
-    signals : SignalId
-        Object instance of class
-    pause : int
-        Amount of time to pause, in seconds
-
-    """
-    while True:
-        read_info = network_api_pb2.SignalIds(signalId=signals)
-        try:
-            response = stub.ReadSignals(read_info)
-            for signal in response.signal:
-                print(f"ecu_B, (read) {signal.id.name} is {get_value(signal)}")
-        except grpc._channel._Rendezvous as err:
-            print(err)
-        time.sleep(pause)
-
-
 def get_value(signal):
     if signal.raw != b"":
         return "0x" + binascii.hexlify(signal.raw).decode("ascii")
@@ -164,7 +95,6 @@ def get_value(signal):
         return signal.arbitration
     else:
         return "empty"
-
 
 def act_on_signal(client_id, stub, sub_signals, on_change, fun, on_subcribed=None):
     sub_info = network_api_pb2.SubscriberConfig(
@@ -191,6 +121,10 @@ def act_on_signal(client_id, stub, sub_signals, on_change, fun, on_subcribed=Non
     # reload, alternatively non-existing signal
     print("subscription terminated")
 
+def act_on_signal_persistant(client_id, stub, sub_signals, on_change, fun, on_subcribed=None):
+    while True:
+        # allow other client to reload
+        act_on_signal(client_id, stub, sub_signals, on_change, fun, on_subcribed)
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Provide address to Beambroker")
@@ -247,8 +181,8 @@ def power_on_client(system_stub, network_stub):
         )
 
     client_id = common_pb2.ClientId(id="id_client_relay")
-    Thread(
-        target=act_on_signal,
+    subscriber = Thread(
+        target=act_on_signal_persistant,
         args=(
             client_id,
             network_stub,
@@ -258,55 +192,9 @@ def power_on_client(system_stub, network_stub):
             False,
             lambda signals: toggle_and_reply(client_id, network_stub, signals),
         ),
-    ).start()
-
-
-def power_on(network_stub):
-
-    client_id = common_pb2.ClientId(id="id_relay")
-
-    Thread(
-        target=act_on_signal,
-        args=(
-            client_id,
-            network_stub,
-            [signal_creator.signal("Relays_resp_set", "physical_relays")],
-            False,
-            lambda signals: (q.put(("physical_relays_responded", signals))),
-            lambda subscripton: (
-                q.put(("physical_relays_subscription_settled", subscripton))
-            ),
-        ),
-    ).start()
-    # wait for subscription to settle
-    (message, subscription) = q.get()
-    assert (message, "physical_relays_subscription_settled")
-
-    def toggle_relays(client_id, network_stub):
-        publish_signals(
-            client_id,
-            network_stub,
-            [
-                signal_creator.signal_with_payload(
-                    "Relay00", "physical_relays", ("integer", 1)
-                ),
-                signal_creator.signal_with_payload(
-                    "Relay01", "physical_relays", ("integer", 2)
-                ),
-            ],
-        )
-    try:
-        toggle_relays(client_id, network_stub)
-        # wait for relay node to reply
-        (message, ignore) = q.get(timeout = 0.1)
-        assert message == "physical_relays_responded"
-        print("power on confirmed")
-        subscription.cancel()
-        return True
-    except:
-        print(f"Relay toggle timeout failed, likely inital setup delay, please retry")
-        subscription.cancel()
-        return False
+    )
+    subscriber.start()
+    return subscriber
 
 
 
@@ -339,75 +227,11 @@ def run(ip, port):
     # Starting Threads
 
     # physical toggle power on using relay.
-    # power_on_client(system_stub, network_stub)
-    while power_on(network_stub) != True:
-        print("Retrying")
+    thread = power_on_client(system_stub, network_stub)
+    thread.join()
 
-    # ecu b, we do this with lambda refering to double_and_publish.
-    def double_and_publish(network_stub, client_id, trigger, signals):
-        for signal in signals:
-            print(f"ecu_B, (subscribe) {signal.id.name} {get_value(signal)}")
-            if signal.id == trigger:
-                publish_signals(
-                    client_id,
-                    network_stub,
-                    [
-                        signal_creator.signal_with_payload(
-                            "counter_times_2",
-                            "ecu_B",
-                            ("integer", get_value(signal) * 2),
-                        ),
-                        # add any number of signals/frames here
-                        # signal_creator.signal_with_payload(
-                        #     "TestFr04", "ecu_B", ("raw", binascii.unhexlify("0a0b0c0d")), False
-                        # )
-                    ],
-                )
-
-    ecu_b_client_id = common_pb2.ClientId(id="id_ecu_B")
-
-    Thread(
-        target=act_on_signal,
-        args=(
-            ecu_b_client_id,
-            network_stub,
-            [
-                signal_creator.signal("counter", "ecu_B"),
-                # here you can add any signal from any namespace
-                # signal_creator.signal("TestFr04", "ecu_B"),
-            ],
-            True,  # True: only report when signal changes
-            lambda signals: double_and_publish(
-                network_stub,
-                ecu_b_client_id,
-                signal_creator.signal("counter", "ecu_B"),
-                signals,
-            ),
-            lambda subscripton: (q.put(("id_ecu_B", subscripton))),
-        ),
-    ).start()
-    # wait for subscription to settle
-    ecu, subscription = q.get()
-
-    # ecu a, this is where we publish, and
-    Thread(
-        target=ecu_A,
-        args=(
-            network_stub,
-            1,
-        ),
-    ).start()
-
-    # ecu b, bonus, periodically, read using timer.
-    signals = [
-        signal_creator.signal("counter", "ecu_B"),
-        # add any number of signals from any namespace
-        # signal_creator.signal("TestFr04", "ecu_B"),
-    ]
-    Thread(target=read_on_timer, args=(network_stub, signals, 1)).start()
-
-    # once we are done we could cancel subscription
-    # subscription.cancel()
+    time.sleep(10)
+    
 
 
 if __name__ == "__main__":
